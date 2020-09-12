1ed102b529b65102e8659334625c4500a19ac506
月守 Jason Kuo <jason.kuo41@gmail.com>
2020-09-12 22:56:28 +0800

    Update nuget settings

---
+++ src/Decuplr.Sourceberg.Generator/Stub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.CodeAnalysis;
+
+namespace Decuplr.Sourceberg.Generator {
+    [Generator]
+    public class AugmentingGenerator : ISourceGenerator {
+        public void Execute(SourceGeneratorContext context) {
+            context.ReportDiagnostic(Diagnostic.Create(new DiagnosticDescriptor("Stub-OK", "Run ok", "Generator has ran", "Generator", DiagnosticSeverity.Info, true), Location.None));
+            return;
+        }
+
+        public void Initialize(InitializationContext context) {
+            return;
+        }
+    }
+}
