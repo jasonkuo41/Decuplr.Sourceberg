c3732f8323a49e714e68058c73192da269932276
月守 Jason Kuo <jason.kuo41@gmail.com>
2020-09-13 10:29:15 +0800

    Add test analyzer

--- src/Decuplr.Sourceberg.Generator/Stub.cs
+++ src/Decuplr.Sourceberg.Generator/Stub.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
 using System.Text;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
 
 namespace Decuplr.Sourceberg.Generator {
     [Generator]
@@ -13,6 +16,20 @@ namespace Decuplr.Sourceberg.Generator {
 
         public void Initialize(InitializationContext context) {
             return;
+        }
+    }
+
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class TestAnalyzer : DiagnosticAnalyzer {
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(new DiagnosticDescriptor("TEST-001","Hey", "Wowie there's a type that ends with test", "Test", DiagnosticSeverity.Warning, true));
+
+        public override void Initialize(AnalysisContext context) {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+            context.RegisterSymbolAction(context => {
+                if (context.Symbol.Name.EndsWith("Test"))
+                    context.ReportDiagnostic(Diagnostic.Create(SupportedDiagnostics[0], context.Symbol.Locations.First()));
+            }, SymbolKind.Method, SymbolKind.NamedType);
         }
     }
 }
